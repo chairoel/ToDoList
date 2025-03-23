5e16ba5b5e6fa2abbe4ae21ae2b9a1abeb6ba327
Chairoel <projectchairul@gmail.com>
2025-03-23 13:47:06 +0700

    Draw the badge symbol

---
+++ ToDoList/View/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  ToDoList
+//
+//  Created by Chairul Amri on 23/03/25.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
