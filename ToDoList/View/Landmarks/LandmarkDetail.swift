33c8f259061a85037640af49a6e784312879d860
Chairoel <projectchairul@gmail.com>
2025-03-23 10:51:49 +0700

    Create a favorite button for each landmark

--- ToDoList/View/Landmarks/LandmarkDetail.swift
+++ ToDoList/View/Landmarks/LandmarkDetail.swift
@@ -9,9 +9,16 @@ import SwiftUI
 import MapKit
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
     
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: {$0.id == landmark.id})!
+    }
+    
     var body: some View {
+        @Bindable var modelData = modelData
+        
         ScrollView {
             MapView(
                 coordinate: landmark.locationCoordinate
@@ -23,8 +30,11 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
 
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
 
                 HStack {
                     Text(landmark.park)
