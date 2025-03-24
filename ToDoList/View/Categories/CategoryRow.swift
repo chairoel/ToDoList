34156ed412cfa3366b8ba658915c1f693fe02686
Chairoel <projectchairul@gmail.com>
2025-03-25 00:09:42 +0700

    Create a category row

---
+++ ToDoList/View/Categories/CategoryRow.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryRow.swift
+//  ToDoList
+//
+//  Created by Chairul Amri on 24/03/25.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+#Preview {
+    let landmarks = ModelData().landmarks
+    return CategoryRow(
+        categoryName: landmarks[0].category.rawValue,
+        items: Array(landmarks.prefix(4))
+    )
+}
